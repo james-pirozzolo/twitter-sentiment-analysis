95bae26fa55859bbf6299a21608f78c75ba4db00
Julian  Croonenberghs <julian@sayhero.com>
2020-11-30 12:42:30 -0500

    cleaned code & increased learning rate

--- code/assignment.py
+++ code/assignment.py
@@ -6,47 +6,42 @@ from preprocess import get_data
 class Model(tf.keras.Model):
     def __init__(self, vocab_size):
         """
-        The Model class predicts the sentiment of a tweet 
+        The Model class computes the sentiment predictions for a batch of tweets 
+
         :param vocab_size: The number of unique words in the data
         """
 
         super(Model, self).__init__()
 
-        # TODO: initialize vocab_size, emnbedding_size
+        # Initializing hyperparameters 
         self.vocab_size = vocab_size
-        # self.window_size = 20 
         self.embedding_size = 300
-        self.learning_rate = 0.001
+        self.learning_rate = 0.01
         self.batch_size = 250 
         # number of output classes 
         self.num_classes = 2
         # LSTM units 
         self.units = 150
 
-        # TODO: initialize embeddings and forward pass weights (weights, biases)
+        # Initializing Adam optimizer 
         self.optimizer = tf.keras.optimizers.Adam(
             learning_rate=self.learning_rate)
 
+        # Initializing Keras layers: embedding, LSTM, and dense layers
         self.embedding_matrix = tf.keras.layers.Embedding(self.vocab_size, self.embedding_size, mask_zero=True)
         self.lstm = tf.keras.layers.LSTM(self.units, return_sequences=True, return_state=True)
-        self.dense_1 = tf.keras.layers.Dense(self.num_classes, activation='relu') # check this 
-        self.dense_2 = tf.keras.layers.Dense(self.num_classes, activation='softmax') # check this 
-        
-        # JUJU begin
-        # might want to use Weights matrix and matmul 
-        # self.W = tf.Variable(tf.random.truncated_normal([self.units, self.units], stddev=.1))
-        # self.dense_2 = tf.keras.layers.Dense(vocab_size)
-        # JUJU end
+        self.dense_1 = tf.keras.layers.Dense(self.num_classes, activation='relu')  
+        self.dense_2 = tf.keras.layers.Dense(self.num_classes, activation='softmax') 
     
     def call(self, inputs, initial_state):
         """
-        ...
+        Performs the forward pass on a batch of tweets to generate the sentiment probabilities.
+        This returns a tensor of shape [batch_size, num_classes], where each row is a
+        probability distribution over the sentiment for tweet.
 
-        :param inputs: batch of tweets of shape (batch_size, tweet_size)
-        :param initial_state: 2-d array of shape (batch_size, rnn_size) as a tensor
-        :return: the batch element probabilities as a tensor, a final_state (Note 1: If you use an LSTM, the final_state will be the last two RNN outputs, 
-        Note 2: We only need to use the initial state during generation)
-        using LSTM and only the probabilites as a tensor and a final_state as a tensor when using GRU 
+        :param inputs: batch of tweets of shape (batch_size, max_length)
+        :param initial_state: 2-d array of shape (batch_size, rnn_size) as a tensor  --> might want to remove this 
+        :return: the batch probabilities tensor, and the last two LSTM states. 
         """
         # get the embeddings of the inputs 
         embedding = self.embedding_matrix(inputs) # shape (batch_size, tweet_size, embedding_size)
@@ -54,116 +49,92 @@ class Model(tf.keras.Model):
         lstm_out, state_1, state_2 = self.lstm(embedding) # shape lstm_out (batch_size, tweet_size, units)
         # because we want to average all vectors to determine sentiment, we reduce_mean on the lstm output
         outputs = tf.reduce_mean(lstm_out, axis=1) # shape outputs (batch_size, units)
-        # apply the dense layer to get logits ((X*W)+b)
-        # JUJU begin
-        # logits = self.dense(outputs)
-        # JUJU out
+        # apply the dense layers to get probabilities
         dense_1_out = self.dense_1(outputs)
         probabilities = self.dense_2(dense_1_out)
-        
-        # JUJU begin
-        # activation function to obtain probabilities 
-        # probabilites = tf.convert_to_tensor(tf.nn.softmax(logits))
 
-        # return logits, probabilities, (state_1, state_2)
-        # JUJU out
-        return dense_1_out, probabilities, (state_1, state_2)
+        return probabilities, (state_1, state_2)
     
-    # def loss(self, logits, labels):
     def loss(self, probs, labels):
         """
-        Calculates average cross entropy sequence to sequence loss of the prediction
+        Calculates the average loss of sentiment predictions for tweets in a given forward pass
 
-        :param logits: a matrix of shape (batch_size, tweet_size, vocab_size) as a tensor
-        :param labels: matrix of shape (batch_size, tweet_size) containing the labels
+        :param probs: a matrix of shape (batch_size, num_classes) as a tensor
+        :param labels: matrix of shape (batch_size,) containing the labels
         :return: the loss of the model as a tensor of size 1
         """
         loss = tf.keras.losses.sparse_categorical_crossentropy(labels, probs)
-        # JUJU begin
-        # loss = tf.keras.losses.sparse_categorical_crossentropy(labels, logits)
-        # JUJU out
         return tf.reduce_mean(loss)
 
     def accuracy(self, probs, labels, print_outputs=False):
         """
-        Calculates the accuracy in testing a sequence of tweets through our model
+        Calculates the batch accuracy of sentiment predictions
 
-        :param probs: probabilities matrix of shape (batch_size, tweet_size) ?
-        :param labels: labels matrix of shape (batch_size, tweet_size)
+        :param probs: probabilities matrix of shape (batch_size, num_classes) 
+        :param labels: labels matrix of shape (batch_size,)
         :return: accuracy of the prediction on the batch of tweets
         """
         decoded_symbols = tf.argmax(input=probs, axis=1)
         if print_outputs:
-            print("probabilities: \n")
+            print("Probabilities:")
             print(probs)
-            print("LABELS")
+            print("Labels:")
             print(labels)
         accuracy = tf.reduce_mean(tf.cast(tf.equal(decoded_symbols, labels), dtype=tf.float32))
         return accuracy
 
 def train(model, train_inputs, train_labels):
     """
-    Runs through one epoch - all training examples.
+    Runs through all training examples and trains the model batch by batch
 
     :param model: the initilized model to use for forward and backward pass
-    :param train_inputs: train inputs (all inputs for training) of shape (num_inputs, tweet_size)
-    :param train_labels: train labels (all labels for training) of shape (num_labels, tweet_size)
+    :param train_inputs: train inputs (all inputs for training) of shape (num_inputs, max_length)
+    :param train_labels: train labels (all labels for training) of shape (num_labels,)
     :return: None
     """
     i = 0
     num_batch = 1
-
-    # # suffle our train data
-    # rows, columns = tf.shape(train_inputs)
-    # indices = tf.random.shuffle(np.arange(rows))
-    # train_inputs = tf.gather(train_inputs, indices) 
-    # train_labels = tf.gather(train_labels, indices)
     num_rows = 50000
-
-    # have to think about how our data is set up here: our tweet lengths are inconsistent.  
     while (i+model.batch_size) < num_rows:
         print("Training batch: " + str(num_batch))
         # batching inputs and labels 
-        ibatch = train_inputs[i: i+model.batch_size] # ibatch shape: (batch_size, max_length=50)
-        lbatch = train_labels[i: i+model.batch_size]  # shape (batch_size)   
+        ibatch = train_inputs[i: i+model.batch_size] 
+        lbatch = train_labels[i: i+model.batch_size]   
         with tf.GradientTape() as tape:
             # forward pass, returning probabilities
-            logits, probs, _ = model.call(ibatch, initial_state=None)
+            probs, _ = model.call(ibatch, initial_state=None)
             # computing loss
-            # loss = model.loss(logits, lbatch)
             loss = model.loss(probs, lbatch)
         # updating gradients
         gradients = tape.gradient(loss, model.trainable_variables)
         model.optimizer.apply_gradients(zip(gradients, model.trainable_variables))
-        # incrementing counter
+        # incrementing counters
         i += model.batch_size
         num_batch += 1
     return None   
 
 def test(model, test_inputs, test_labels):
     """
-    Runs through one epoch - all testing examples
+    Runs through all testing examples and tests the model batch by batch
 
     :param model: the trained model to use for prediction
-    :param test_inputs: train inputs (all inputs for testing) of shape (num_inputs,)
-    :param test_labels: train labels (all labels for testing) of shape (num_labels,)
-    :returns: perplexity of the test set
+    :param test_inputs: test inputs (all inputs for testing) of shape (num_inputs, max_length)
+    :param test_labels: test labels (all labels for testing) of shape (num_labels,)
+    :returns: accuracy and average loss of the model on the test set 
     """
     total_loss = 0
     total_acc = 0
     num_batches = 0
     i = 0
-    # have to think about how our data is set up here: our tweet lengths are inconsistent.  
     while (i+model.batch_size) < len(test_inputs):
         print("Testing batch: " + str(num_batches + 1))
         # batching inputs and labels
         ibatch = test_inputs[i: i+model.batch_size]
         lbatch = test_labels[i: i+model.batch_size]
         # forward pass, returning probabilities
-        logits, probs, _ = model.call(ibatch, initial_state=None)
+        probs, _ = model.call(ibatch, initial_state=None)
         # computing loss 
         loss = model.loss(probs, lbatch)
-        # loss = model.loss(logits, lbatch)
         total_loss += loss
         # computing accuracy
         if i == 0:
@@ -180,17 +151,17 @@ def test(model, test_inputs, test_labels):
     return avg_acc, avg_loss
 
 def main():
-    # TO-DO: Pre-process and vectorize the data
+    # Pre-process the data
     train_inputs, train_labels, test_inputs, test_labels, vocab_dict = get_data(
-        '../data/train_mini.csv', '../data/test.csv')
+        '../data/first_50k.csv', '../data/test.csv')
 
-    # TODO: initialize model and tensorflow variables
+    # Initialize the model and tensorflow variables 
     model = Model(len(vocab_dict))
 
-    # TODO: Set-up the training step
+    # Train the model on the train data 
     train(model, train_inputs, train_labels)
 
-    # TODO: Set up the testing steps
+    # Test the model on the test data
     accuracy, loss = test(model, test_inputs, test_labels)
 
     # print the accuracy!! 
