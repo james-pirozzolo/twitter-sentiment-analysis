5b6bffacad1daa81774ca17e098d4dceabfe8cdd
Will Kantaros <wkantaros@gmail.com>
2020-12-07 21:12:23 -0500

    beginning website

--- code/repl.py
+++ code/repl.py
@@ -28,24 +28,31 @@ def load_model(filepath):
     vocab = np.load(filepath + '/vocab.npy', allow_pickle='TRUE').item()
     return (embedding, lstm, dense_1, dense_2, vocab)
 
-# def get_tweet_sentiment(model,vocab,raw_tweet):
-#     # going to need to preprocess 'tweet'
-#     cleaned_tweet = clean_tweet(raw_tweet)
-#     padded_tweet = pad_corpus([cleaned_tweet])
-#     tweet = convert_to_id(padded_tweet, vocab)
-#     probs, _ = model.call(tweet, initial_state=None)
-#     # print(probs)
-#     return probs
-
-def get_tweet_sentiment_from_filepath(raw_tweet, filepath):
-    embedding, lstm, dense_1, dense_2, vocab = load_model(filepath)
-    model = LoadedModel(embedding, lstm, dense_1, dense_2)
-    probs,_ = get_tweet_sentiment(model, vocab, raw_tweet)
+def get_tweet_sentiment(model,vocab,raw_tweet):
+    # going to need to preprocess 'tweet'
+    cleaned_tweet = clean_tweet(raw_tweet)
+    padded_tweet = pad_corpus([cleaned_tweet])
+    tweet = convert_to_id(padded_tweet, vocab)
+    probs, _ = model.call(tweet, initial_state=None)
+    # print(probs)
     return probs
 
+def get_tweet_sentiment_from_filepath(raw_tweet, filepath):
+    # returns negative sentiment
+    embedding, lstm, dense_1, dense_2, vocab = load_model(filepath)
+    model = LoadedModel(embedding, lstm, dense_1, dense_2)
+    probs = get_tweet_sentiment(model, vocab, raw_tweet)
+    return probs.numpy()[0][0]
+
 def main():
+    # print('here!')
+    # filepath = '../saved_model'
+    # tweet = "i hate lebron james"
     prbs = get_tweet_sentiment_from_filepath(sys.argv[1], sys.argv[2])
+    # prbs = get_tweet_sentiment_from_filepath(tweet, filepath)
     print(prbs)
+    # print('hello world')
+    # sys.stdout.flush()
 
 if __name__ == '__main__':
     main()
